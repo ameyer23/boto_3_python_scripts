b7b841b89474edaf3b31b5d972112904bd2f1f38
Andrea Meyer Munoz <andreameyer23@gmail.com>
2023-08-15 02:38:16 +0000

    list_buckets

--- list_buckets.py
+++ list_buckets.py
@@ -1,11 +1,13 @@
 import boto3
 
-s3 = boto3.client('s3') #we set client variable to be s3, the resource
+s3 = boto3.client('s3') #we set client variable to be s3, the resource.
 
 response = s3.list_buckets()
 
-buckets = response['Buckets'] #contians all info from buckets
+#contians all info from buckets
+#this is not prettyprinted, do so to more easily see details 
+buckets = response['Buckets'] 
 
-for bucket in buckets: #lists bucket names and creation data from our account
-    print (bucket['Name'], bucket['CreationDate'])
+
+print(buckets)
 
