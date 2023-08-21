5289a92337b37d7ea231b9df9cc184d8d7f60814
Andrea Meyer Munoz <andreameyer23@gmail.com>
2023-08-21 03:54:51 +0000

    associate internet gateway

---
+++ VPC/create_internet_gateway_attachment.py
@@ -0,0 +1,14 @@
+"""
+Your module description
+"""
+import boto3
+
+ec2= boto3.client('ec2')
+
+igw_id = 'igw-0adc877990bffd96d'
+vpc_id = 'vpc-06a56934a663a6d4d'
+
+ec2.attach_internet_gateway(
+    InternetGatewayId=igw_id,
+    VpcId=vpc_id
+)
